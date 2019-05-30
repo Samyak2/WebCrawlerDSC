869bb6de9146579fe42923b1227cc848ff6b0ae7
Samyak S Sarnayak <samyak201@gmail.com>
2019-05-30 17:57:09 +0530

    Fixed bugs

--- image_scraper.py
+++ image_scraper.py
@@ -24,12 +24,12 @@ class ImageSpider(scrapy.Spider):
         for imagecolumn in response.css(".photos__column"):#response.xpath("//div[@class=\"photos__column\"]"):
             if(not imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]")):
                 break
-            img_url = (imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]/img/@data-big-src").extract_first()).split(".jpeg")[0].split(".jpg")[0] + ".jpg"
+            img_url = (imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]/img/@data-big-src").extract_first())#.replace(".jpeg", ".jpg").split(".jpg")[0] + ".jpg"
             img_count += 1
             yield {
                 "image": img_url
             }
-            urllib.request.urlretrieve(img_url, "Images/" + self.wordlist[ImageSpider.count] + " " + img_count)
+            urllib.request.urlretrieve(img_url, "Images/" + self.wordlist[ImageSpider.count] + " " + str(img_count) + ".jpg")
         if(self.wordlist[ImageSpider.count]):
             ImageSpider.count += 1
             yield scrapy.Request(response.urljoin(
