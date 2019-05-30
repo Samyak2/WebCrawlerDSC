a03cf6ca68505c73fbf132254af8a4063b23d0fa
Samyak S Sarnayak <samyak201@gmail.com>
2019-05-30 17:12:11 +0530

    Extracts images related to recipe

--- image_scraper.py
+++ image_scraper.py
@@ -4,7 +4,13 @@ import csv
 
 class ImageSpider(scrapy.Spider):
     name = "image_spider"
-    search_term = "butter"
+    wordlist = []
+    with open("wordlist.csv", "r") as csvfile:
+        readCSV = csv.reader(csvfile)
+        for row in readCSV:
+            wordlist += row
+    count = 0
+    search_term = wordlist[count]
     start_urls = ["https://www.pexels.com/search/" + search_term]
 
     opener=urllib.request.build_opener()
@@ -12,10 +18,19 @@ class ImageSpider(scrapy.Spider):
     urllib.request.install_opener(opener)
 
     def parse(self, response):
+        print("NOW SEARCHINGGGGGG: " + self.wordlist[ImageSpider.count])
         img_url = ""
+
         for imagecolumn in response.css(".photos__column"):#response.xpath("//div[@class=\"photos__column\"]"):
+            if(not imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]")):
+                break
             img_url = (imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]/img/@data-big-src").extract_first()).split(".jpeg")[0] + ".jpeg"
             yield {
                 "image": img_url
             }
-            urllib.request.urlretrieve(img_url, img_url.split("/")[-1])
+            urllib.request.urlretrieve(img_url, "Images/" + self.wordlist[ImageSpider.count] + " " + img_url.split("/")[-1])
+        if(self.wordlist[ImageSpider.count]):
+            ImageSpider.count += 1
+            yield scrapy.Request(response.urljoin(
+                "https://www.pexels.com/search/" + self.wordlist[ImageSpider.count]),
+                callback=self.parse)
