e124c9643f40af63f705fd0d85fbab85bdb1884c
Samyak S Sarnayak <samyak201@gmail.com>
2019-05-31 11:08:13 +0530

    Formatting

--- image_scraper.py
+++ image_scraper.py
@@ -2,6 +2,7 @@ import scrapy
 import urllib.request
 import csv
 
+
 class ImageSpider(scrapy.Spider):
     name = "image_spider"
     wordlist = []
@@ -13,23 +14,28 @@ class ImageSpider(scrapy.Spider):
     search_term = wordlist[count]
     start_urls = ["https://www.pexels.com/search/" + search_term]
 
-    opener=urllib.request.build_opener()
-    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    opener = urllib.request.build_opener()
+    opener.addheaders = [
+        ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
     urllib.request.install_opener(opener)
 
     def parse(self, response):
         print("NOW SEARCHINGGGGGG: " + self.wordlist[ImageSpider.count])
         img_url = ""
         img_count = 0
-        for imagecolumn in response.css(".photos__column"):#response.xpath("//div[@class=\"photos__column\"]"):
+        # response.xpath("//div[@class=\"photos__column\"]"):
+        for imagecolumn in response.css(".photos__column"):
             if(not imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]")):
                 break
-            img_url = (imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]/img/@data-big-src").extract_first())#.replace(".jpeg", ".jpg").split(".jpg")[0] + ".jpg"
+            # .replace(".jpeg", ".jpg").split(".jpg")[0] + ".jpg"
+            img_url = (imagecolumn.xpath(
+                ".//a[@class=\"js-photo-link photo-item__link\"]/img/@data-big-src").extract_first())
             img_count += 1
             yield {
                 "image": img_url
             }
-            urllib.request.urlretrieve(img_url, "Images/" + self.wordlist[ImageSpider.count] + " " + str(img_count) + ".jpg")
+            urllib.request.urlretrieve(
+                img_url, "Images/" + self.wordlist[ImageSpider.count] + " " + str(img_count) + ".jpg")
         if(self.wordlist[ImageSpider.count]):
             ImageSpider.count += 1
             yield scrapy.Request(response.urljoin(
