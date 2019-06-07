5e5620dc8c237689fb34abaa4cfc0a11c6bcbc5d
Samyak S Sarnayak <samyak201@gmail.com>
2019-06-07 11:56:25 +0530

    Updated YouTube scraper to return urls

--- youtube_video_scraper.py
+++ youtube_video_scraper.py
@@ -1,9 +1,13 @@
 import scrapy
+import os
+import subprocess
 
 class YouTubeSpider(scrapy.Spider):
     name = "youtube_spider" #spider name
-    search_term = "video"
-    start_urls = ["https://www.youtube.com/results?search_query=" + search_term] #url to scrape
+    # search_term = "video"
+    def __init__(self, search_term):
+        self.search_term = search_term
+        self.start_urls = ["https://www.youtube.com/results?search_query=" + self.search_term] #url to scrape
     
     #to set user agent (so that they don't block us for using scrapy and so that it youtube thinks we are using a normal browser)
     def start_requests(self):
@@ -12,5 +16,16 @@ class YouTubeSpider(scrapy.Spider):
     def parse(self, response):
         #get video links and print
         video_urls = response.xpath("//h3//a/@href").extract()
-        video_urls = ["https://www.youtube.com" + i for i in video_urls] #add base url to each link (because the links we got are relative)
-        print(video_urls)
+        video_urls = ["https://www.youtube.com" + i for i in video_urls if "watch" in i] #add base url to each link (because the links we got are relative)
+        for video_url in video_urls:
+            print(video_url)
+
+def runYouTubeSpider(search_term):
+    # os.system("scrapy runspider youtube_video_scraper.py --nolog -a search_term=" + search_term)
+    op = subprocess.check_output("scrapy runspider youtube_video_scraper.py --nolog -a search_term=" + search_term, shell=True, universal_newlines=True)
+    op = op.split("\n")
+    print(op)
+    return op
+
+if __name__ == "__main__":
+    runYouTubeSpider("pewdiepie")
